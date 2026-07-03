20a044a263bb407f203adb44c2999065040ba566
RebEmnacin <rebqemnacin@gmail.com>
2026-07-03 12:12:30 +0800

    Updated navigation titles theme

--- src/js/bookshop.js
+++ src/js/bookshop.js
@@ -28,7 +28,7 @@ if (book) {
         Back
       </a>
     </div>
-    <p style="margin: 50px; font-family: Bebas Neue;">Book not found.</p>
+    <p style="margin: 50px; font-family: M PLUS 1p;">Book not found.</p>
   `;
 }
 
